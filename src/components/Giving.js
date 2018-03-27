d5c58bd51a9aa7de13e398a0f59e64f549655f4d
Joel Pratt <pratt.joel@gmail.com>
2018-03-26 22:56:26 -0400

    Add link to website on giving page.

--- src/components/Giving.js
+++ src/components/Giving.js
@@ -1,10 +1,41 @@
 'use-strict';
 
 import React from 'react';
-import { Text } from 'react-native';
+import {
+    Text,
+    View,
+    Linking
+} from 'react-native';
 
 const Giving = () => {
-  return <Text>Giving</Text>;
+    const givingLink = (
+        <View style={styles.containerStyle}>
+            <Text
+                onPress={openGivingUrl}
+                style={styles.givingLinkStyle}
+            >
+                    Give
+            </Text>
+        </View>
+    );
+  return givingLink;
+};
+
+const givingUrl = 'http://www.newdaychurch.nyc/contribute/';
+
+const openGivingUrl = () => {
+    Linking.openURL(givingUrl);
 };
 
 export default Giving;
+
+const styles = {
+    containerStyle: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    givingLinkStyle: {
+        fontSize: 46
+    }
+};
