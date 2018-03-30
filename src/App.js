5d9e6f2f725b09af9362f31eeff606c8bd2da1e2
Joel Pratt <pratt.joel@gmail.com>
2018-03-30 13:07:53 -0400

    Clear image cache from previous session when app loads.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Expo from 'expo';
+import Expo, { FileSystem } from 'expo';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -9,6 +9,7 @@ import {
   createReduxBoundAddListener,
   createReactNavigationReduxMiddleware,
 } from 'react-navigation-redux-helpers';
+import { CacheManager } from 'react-native-expo-image-cache';
 import reducers from './reducers';
 // import Router from './Nav/Router';
 import MainNavigation from './containers/NavContainer';
@@ -17,6 +18,16 @@ console.ignoredYellowBox = ['Remote debugger'];
 
 export default class App extends Component {
   componentWillMount() {
+      // console.log('App component mounted');
+      // const BASE_DIR = `${FileSystem.cacheDirectory}expo-image-cache/`;
+      // const currentFiles = FileSystem.readDirectoryAsync(BASE_DIR);
+      // console.log('files before clear cache: ', currentFiles);
+      CacheManager.clearCache();
+
+      // const filesAfterCacheClear = FileSystem.readDirectoryAsync(BASE_DIR);
+      // console.log('files after clear cache: ', filesAfterCacheClear);
+
+
     const config = {
       apiKey: 'AIzaSyBS012kYgoyxm6ig-ldjhVUElcPldg1wE4',
       authDomain: 'communityconnect-aa662.firebaseapp.com',
@@ -27,6 +38,10 @@ export default class App extends Component {
     };
     firebase.initializeApp(config);
   }
+
+  // componentWillUnMount() {
+  //     console.log('App component unmounted');
+  // }
 
   render() {
       const middleware = createReactNavigationReduxMiddleware(
